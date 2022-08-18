ac188107285c71dbc6cda334f5dbe64dcb9081df
kishan <kishanbapodra2002@gmail.com>
2022-08-18 21:21:09 +0400

    Add: helper functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
  
-function App() {
+type Sandwich = {
+  fBread: String,
+  fCheese: String,
+  fVeggies: String[],
+  fCondiments: String[],
+}
+
+const App: React.FC = () => {
+  
+  const [sandwich, setSandwich] = useState<Sandwich>({fBread: "", fCheese: "", fVeggies: [], fCondiments: []});
+  
+  const breadType = (bread: String) => {
+    setSandwich({...sandwich, fBread: bread});
+  }
+
+  const cheeseType = (cheese: String) => {
+    setSandwich({...sandwich, fCheese: cheese })
+  }
+
+  const addVeggie = (veggie: String) => {
+    
+    let newVeggie: String[];
+    if(!sandwich.fVeggies.includes(veggie)){
+    newVeggie = [...sandwich.fVeggies, veggie];
+    } else {
+      newVeggie = sandwich.fVeggies.filter(veg => veg !== veggie)
+    }
+    setSandwich({...sandwich, fVeggies: newVeggie});
+  }
+
+  const addCondiment = (condiment: String) => {
+
+    let newCondiment
+    if(!sandwich.fCondiments.includes(condiment)) {
+      newCondiment = [...sandwich.fCondiments, condiment];
+    } else {
+      newCondiment = sandwich.fCondiments.filter(sauce => sauce !== condiment);
+    }
+    setSandwich({...sandwich, fVeggies: newCondiment});
+  }
+
   return (
     <>
       <Routes>
