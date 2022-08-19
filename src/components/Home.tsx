8556e890bb2dda11c0ef7793ca83f681c90f6b55
kishan <kishanbapodra2002@gmail.com>
2022-08-20 02:44:31 +0400

    Style: home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css'
 
 const Home = () => {
   return (
     <div className='home container'>
-      <h2>WELCOME TO YOUR NEIGHBOURHOOD SUBWAY JOINT</h2>
+      <h2 className='header'>WELCOME TO YOUR NEIGHBOURHOOD SUBWAY JOINT</h2>
       <Link to="/bread">
-        <button>Create your Sandwich</button>
+        <button className='btn'>Create your Sandwich</button>
       </Link>
     </div>
   );
