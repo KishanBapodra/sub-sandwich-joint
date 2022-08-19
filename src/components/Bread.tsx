021909d37d53f6c071d530d082eb9c845ef6107d
kishan <kishanbapodra2002@gmail.com>
2022-08-19 16:10:49 +0400

    Feat: bread selection

--- src/components/Bread.tsx
+++ src/components/Bread.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Sandwich } from '../App';
+import { Link } from 'react-router-dom'
 
 type BreadProps = {
   breadType: (bread: String) => void,
@@ -7,11 +8,34 @@ type BreadProps = {
 }
 
 const Bread = ({breadType, sandwich}: BreadProps) => {
+
+  const breads: String[] = ['Italian','Wheat','Parmesan Oregano','Honey Oat']
+
   return (
-    <div>
-      BREAD
-    </div>
-  );
+    <>
+      <div className='bread container'>
+        <h3>Choose your Bread</h3>
+        <ul>
+          {breads.map(bread => {
+            let active = sandwich.fBread === bread ? 'active' : '';
+            return(
+              <li onClick={()=> breadType(bread)}>
+                <span className={active}>{bread}</span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      {sandwich.fBread && (
+        <div className='next'>
+          <Link to="/cheese">
+            <button>Next</button>
+          </Link>
+        </div>
+      )}
+    </>
+  )
 }
 
 export default Bread;
