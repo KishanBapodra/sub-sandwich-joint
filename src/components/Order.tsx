02449e30bc3364e5b62ffb80c75d075b476365e7
kishan <kishanbapodra2002@gmail.com>
2022-08-20 19:50:04 +0400

    Feat: button to redirect to home

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Sandwich } from '../App';
 
 const Order = ({sandwich}: {sandwich: Sandwich}) => {
   return (
     <div className='order container'>
         <h2>Thank you for your order &#128523;</h2>
-        <p>You have ordered a Sandwich with {sandwich.fBread} bread as the base and {sandwich.fCheese} cheese with the following veggies: </p>
+        <p>You have ordered a Sandwich with {sandwich.fBread} bread as the base and {sandwich.fCheese} with the following veggies: </p>
         <p>{sandwich.fVeggies.map(veg => <>{veg} &ensp;</>)}</p> and the following condiments:
         <p>{sandwich.fCondiments.map(condiment => <>{condiment} &ensp;</>)}</p>
-        
+        <button>
+          <Link to='/'>Order another</Link>
+        </button>
     </div>
   );
 }
