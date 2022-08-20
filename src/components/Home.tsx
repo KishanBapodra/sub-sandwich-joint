58afe023efc01fc5b9dbb816440d6bf6554d9b9b
kishan <kishanbapodra2002@gmail.com>
2022-08-20 19:47:25 +0400

    Style: neonish text
    change minor styles and added neon effect to text

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ import './Home.css'
 const Home = () => {
   return (
     <div className='home container'>
-      <h2 className='header'>WELCOME TO YOUR NEIGHBOURHOOD SUBWAY JOINT</h2>
+      <h2 className='header'>WELCOME TO SUBWAY JOINT</h2>
       <Link to="/bread">
         <button className='btn'>Create your Sandwich</button>
       </Link>
