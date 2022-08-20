eac92df96e7833fe174916aad939e163d1bf43f0
kishan <kishanbapodra2002@gmail.com>
2022-08-21 00:21:43 +0400

    Style: list styling

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Sandwich } from '../App';
+import styles from './List.module.css'
 
 type ListProps = {
     mapItem: String[],
@@ -10,19 +11,19 @@ type ListProps = {
 
 const List = ({mapItem, type, sandwich, func}: ListProps) => {
   return (
-    <>
-    <h3>Choose your {type}</h3>
-    <ul>
-      {mapItem.map(item => {
-          let active = sandwich.fBread === item || sandwich.fCheese === item || sandwich.fVeggies.includes(item) || sandwich.fCondiments.includes(item) ? 'active' : '';
-          return (
-              <li onClick={() => func(item)}>
-                <span className={active}>{item}</span>
-            </li>
-        );
-    })}
-    </ul>
-    </>
+    <div>
+      <h3 className={styles.pageTitle}>Step 1: Choose your {type}</h3>
+      <ul className={styles.list}>
+        {mapItem.map(item => {
+            let active = sandwich.fBread === item || sandwich.fCheese === item || sandwich.fVeggies.includes(item) || sandwich.fCondiments.includes(item) ? true : false;
+            return (
+                <li className={styles.listItem} onClick={() => func(item)}>
+                  <span className={active ? styles.isActive : ''}>{item}</span>
+              </li>
+          );
+      })}
+      </ul>
+    </div>
   );
 }
 
