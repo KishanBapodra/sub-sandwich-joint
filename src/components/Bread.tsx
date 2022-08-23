83769fac444bca5bf461f622f53cc0acdfedfc38
kishan <kishanbapodra2002@gmail.com>
2022-08-23 08:50:05 +0400

    Optimize: variants framer motion

--- src/components/Bread.tsx
+++ src/components/Bread.tsx
@@ -14,25 +14,59 @@ const Bread = ({breadType, sandwich}: BreadProps) => {
 
   const breads: String[] = ['Italian','Wheat','Parmesan Oregano','Honey Oat']
 
+  const containerVariant = {
+    hidden: {
+      x: '60vw'
+    },
+    visible: {
+      x:0,
+      transition: {
+        duration: 0.25
+      }
+    }
+  }
+
+  const btnVariant = {
+    hidden: {
+      x: '-70vw',
+      opacity: 0
+    },
+    visible: {
+      x: 40,
+      opacity: 1,
+      transiton: {
+        delay: 2,
+        duration: 3,
+        type: 'spring',
+        stiffness: 75,
+      }
+    },
+    hover: {
+      scale: 1.1,
+      boxShadow: "0 0 4px #FF4996",
+      textShadow: "0 0 2px #FFF",
+    }
+  }
+
   return (
     <>
-      <motion.div initial={{x:'60vw'}} animate={{x:0}} transition={{duration: 0.25}} className={styles.container}>
+      <motion.div className={styles.container} variants={containerVariant} initial="hidden" animate="visible">
         
         <List mapItem={breads} type="Bread" sandwich={sandwich} func={breadType} />
 
-        {sandwich.fBread && <div className={styles.btn}>
+        {/* {sandwich.fBread &&  */}
+        <div className={styles.btn}>
           <Link to="/cheese">
-          <motion.button 
-                initial={{x:'-70vw'}}
-                animate={{x:0}}
-                transition={{type: 'spring', stiffness: 75}}
-                whileHover={{scale: 1.1, boxShadow: "0 0 4px #FF4996", textShadow: "0 0 2px #FFF" }}
+          <motion.button
+                variants={btnVariant}
+                whileHover="hover"
                 className={sandwich.fBread.length !== 0 ? styles.next : styles.hid}
               >
                 Next
               </motion.button>
           </Link>
-        </div>}
+        </div>
+         {/* } */}
 
       </motion.div>
     </>
